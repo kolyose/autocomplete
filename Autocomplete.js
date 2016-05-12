a8ffc1a6e81e6df063723ed4eac847f3cee29213
kolyose <kolyose@gmail.com>
2016-05-12 23:52:48 +0300

    spinner added

--- Autocomplete.js
+++ Autocomplete.js
@@ -23,29 +23,19 @@ class Autocomplete{
 	}	
 	
 	setOptions(options){
-		
 		if (!options || !(options instanceof Array)) return;
 			
 		for (let i=this._select.children.length-1; i>-1 ; i--){
 			this._select.children[i].remove();
     	}
 		
-	/*	let defaultOption = new Option(this._input.value);
-		defaultOption.selected = true;
-		defaultOption.disabled = true;
-		this._select.appendChild(defaultOption);*/
-		
 		options.forEach((optionData) =>{
 			var option = new Option(optionData.label, optionData.value);
-			option.selected = false;
-			option.onkeydown = () => {alert("focus")}
 			this._select.appendChild(option);
 		});		
 		
-		this._showDropdown();
-		
-		var option = this._select.querySelector('option');
-		option.onfocus = function (){console.log("option")}
+		this._showDropdown();		
+		this._updateInputValue();
 	}
 	
 	getValue(){
@@ -57,19 +47,20 @@ class Autocomplete{
 		this._select = this.node.querySelector('select');	
 		this._input = this.node.querySelector('input');	 
 		
-		//this._select.onclick = () => {console.log("clock")}
-		
 		this._select.onchange = (event) => {
-		//	console.log("input")
-		//	console.log(this._select.selectedIndex)
-			this._input.value = this._select.options[this._select.selectedIndex].label;
+			this._updateInputValue();
 			if (this._onChange) this._onChange(this.getValue());
 		}
+	
 	}
 	
 	_showDropdown() {		
     	const event = new MouseEvent('mousedown');
     	this._select.dispatchEvent(event);
+	}
+	
+	_updateInputValue(){
+		this._input.value = this._select.options[this._select.selectedIndex].label;
 	}
 	
 	_createNode(){		
@@ -79,6 +70,7 @@ class Autocomplete{
 				<select>
 				</select>
 				<input type="text"/>				
+ 				<div id="spinner"></div>
 			</form>`;	
 	}
 }
